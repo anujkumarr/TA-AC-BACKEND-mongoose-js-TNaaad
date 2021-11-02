0644b1332be64befc79db49a35675628d3bbd2a8
akk26 <anujbph@gmail.com>
2021-11-02 17:53:34 +0530

    Completed

--- block-BNaacd/app/app.js
+++ block-BNaacd/app/app.js
@@ -2,6 +2,5 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/app', (err) => {
-  
   console.log(err ? err : 'Connected to the database');
 });
